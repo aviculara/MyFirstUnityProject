3c7b1a19116e719c514cc8d6502329aa1f9da1ca
Lara <114432707+aviculara@users.noreply.github.com>
2024-02-28 22:26:07 +0300

    Update HelloWorld.cs

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //i am writing this from github.com
     }
 
     // Update is called once per frame
