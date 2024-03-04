327d29a0bf473528ed696eda610a3bb504fc6704
Lara <114432707+aviculara@users.noreply.github.com>
2024-03-04 23:00:43 +0300

    w2 pazartesi sonu

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.AI;
+using TMPro;
+
 
 public class HelloWorld : MonoBehaviour
 {
+    public int speed = 10;
+    int look = 1_000; //okunabilmesi için , yerine _ koyabilirsin
+    byte levelNo = 1; //küçük sayılar için byte ile tutabilirsin
+    string myText = "Hello World";
+
+    public GameObject helloText;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        print("hello world");
         //now i am writing from vs
         //i am writing this from github.com
+        Application.targetFrameRate = 30;
+        //framerate sabitlemek gerektiğinde
+        helloText.GetComponent<TextMeshPro>().text = myText;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed); //istediğim bir hızda gidiyor şimdi
     }
 }
